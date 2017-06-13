6d1e910a9c36026d18fab4b5c59ce9914e2d8d1c
Eduardo Cucharro <eduardo.cucharro@gmail.com>
2017-06-13 15:16:37 -0300

    salva dados cartao shared preferences

--- app/src/main/java/br/com/cucha/meucartao/EditorActivity.kt
+++ app/src/main/java/br/com/cucha/meucartao/EditorActivity.kt
@@ -1,12 +1,39 @@
 package br.com.cucha.meucartao
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class EditorActivity : AppCompatActivity() {
+
+    lateinit var editNome: EditText
+    lateinit var editEmail: EditText
+    lateinit var editFone: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
+
+        editNome = findViewById(R.id.edit_nome_editor) as EditText
+        editEmail = findViewById(R.id.edit_email_editor) as EditText
+        editFone = findViewById(R.id.edit_fone_editor) as EditText
+
+        findViewById(R.id.button_salvar_editor).setOnClickListener {
+            salvaCartao()
+        }
+    }
+
+    fun salvaCartao() {
+        val preferences = getPreferences(Context.MODE_PRIVATE)
+
+        val editor = preferences.edit()
+        editor.putString("NOME", editNome.text.toString())
+        editor.putString("EMAIL", editEmail.text.toString())
+        editor.putString("FONE", editFone.text.toString())
+
+        editor.apply()
+
+        finish()
     }
 }
