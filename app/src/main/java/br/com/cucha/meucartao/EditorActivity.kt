265026f9d6d96032ba2ebf7b12682cf8fd44a3b8
Eduardo Cucharro <eduardo.cucharro@gmail.com>
2017-06-13 14:54:16 -0300

    setup projeto com koltin e leiaute das duas activities

---
+++ app/src/main/java/br/com/cucha/meucartao/EditorActivity.kt
@@ -0,0 +1,12 @@
+package br.com.cucha.meucartao
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EditorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editor)
+    }
+}
